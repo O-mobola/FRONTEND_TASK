c9e42f11595a0dce3749b1fa97e780cfaaa8f34f
O-mobola <ssalawudeen06@gmail.com>
2020-03-24 10:31:00 +0100

    initial commit to this branch forjavascript task

---
+++ script.js
@@ -0,0 +1,14 @@
+let name, oddNumber;
+
+name = "Saudat Salawudeen";
+
+const courses = ["HTML", " CSS", " JavaScript", " PHP", " Python"];
+
+console.log("My name is "+name+" and my courses are "+courses);
+
+for (oddNumber = 0; oddNumber <= 200; oddNumber++) {
+  if(oddNumber%2!==0){
+    console.log(oddNumber);
+  }
+  
+}
